f0daa21e4b5f3ab5cf233b02a29a9cb88121679e
conner-mcnicholas <connermcnicholas@gmail.com>
2025-10-23 00:57:59 -0700

    fixing the configs and rules

--- keyboards/ferris/keymaps/finalferriskey34/config.h
+++ keyboards/ferris/keymaps/finalferriskey34/config.h
@@ -16,31 +16,25 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #pragma once
-// Set the mouse settings to a comfortable speed/accuracy trade-off,
-// assuming a screen refresh rate of 60 Htz or higher
-// The original (from Conner's perspective) settings felt too fast, bringing default of 10 down to 5.
-#define MOUSEKEY_MAX_SPEED 7
-// The default is 50. This makes the mouse ~3 times faster and more accurate
-#define MOUSEKEY_INTERVAL 8
-#define MOUSEKEY_MOVE_DELTA 2
-// The default is 20. Since we made the mouse about 3 times faster with the previous setting,
-// give it more time to accelerate to max speed to retain precise control over short distances.
-#define MOUSEKEY_TIME_TO_MAX 20
-// The default is 300. Let's try and make this as low as possible while keeping the cursor responsive
-#define MOUSEKEY_DELAY 50
-// It makes sense to use the same delay for the mouseweel
-#define MOUSEKEY_WHEEL_DELAY 50
-// The default is 100
-#define MOUSEKEY_WHEEL_INTERVAL 50
-// The default is 40
-#define MOUSEKEY_WHEEL_TIME_TO_MAX 100
+
+#define MOUSEKEY_DELAY	10 //Delay between pressing a movement key and cursor movement
+#define MOUSEKEY_INTERVAL	20	//Time between cursor movements in milliseconds
+#define MOUSEKEY_MOVE_DELTA	8	//Step size
+#define MOUSEKEY_MAX_SPEED	10	//Maximum cursor speed at which acceleration stops
+#define MOUSEKEY_TIME_TO_MAX	30	//Time until maximum cursor speed is reached
+
+#define MOUSEKEY_WHEEL_DELAY	10	//Delay between pressing a wheel key and wheel movement
+#define MOUSEKEY_WHEEL_INTERVAL	80	//Time between wheel movements
+#define MOUSEKEY_WHEEL_DELTA	1	//Wheel movement step size
+#define MOUSEKEY_WHEEL_MAX_SPEED	8	//Maximum number of scroll steps per scroll action
+#define MOUSEKEY_WHEEL_TIME_TO_MAX	40	//Time until maximum scroll speed is reached
 
 // Pick good defaults for enabling homerow modifiers
-#define TAPPING_TERM 200
+#define TAPPING_TERM 280
 //#define PERMISSIVE_HOLD
-#define QUICK_TAP_TERM 200
+#define QUICK_TAP_TERM 280
 
-#define FLOW_TAP_TERM 150
+#define FLOW_TAP_TERM 280
 
 #define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
 
